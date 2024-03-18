ed5e7eabc3c5cd57b6c28ef68f6f1fbe2a7858fd
mohs-in <in.mohsin@outlook.com>
2024-03-18 12:18:41 +0530

    save created notes to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from "react"
-import Sidebar from "./components/Sidebar"
-import Editor from "./components/Editor"
+import Sidebar from "/components/Sidebar.jsx"
+import Editor from "/components/Editor.jsx"
 import { data } from "./data"
 import Split from "react-split"
 import {nanoid} from "nanoid"
 
 export default function App() {
-    const [notes, setNotes] = React.useState([])
+    const [notes, setNotes] = React.useState(JSON.parse(localStorage.getItem("notes")) || [])
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
     
+    React.useEffect(() => {
+        localStorage.setItem("notes",JSON.stringify(notes))
+    },[notes])
+
     function createNewNote() {
+        console.log('created')
         const newNote = {
             id: nanoid(),
             body: "# Type your markdown note's title here"
